1eacfb3bb6069dbc67d44a5b4b2675f353173db4
Abhradeep De <68565872+abhra2020-smart@users.noreply.github.com>
2021-08-04 21:45:11 +0100

    Add files via upload

--- components/Header.js
+++ components/Header.js
@@ -1,9 +1,13 @@
-function Header() {
+import Image from 'next/image';
+
+export default function Header() {
     return (
-        <div>
+        <header className='sticky top-0 z-50'>
+            <div className='relative flex items-center h-10 cursor-pointer my-auto'>
+                <Image src="https://links.papareact.com/qd3" layout='fill' objectFit='contain' objectPosition='left'/>
+            </div>
             <div></div>
             <div></div>
-            <div></div>
-        </div>
+        </header>
     );
 }
